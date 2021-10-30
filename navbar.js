f0ff173013ca4c49bf8a40ba9b1036e0d669c9eb
Sakline Sk <Sakline_pw1_056@masai.school>
2021-10-30 21:04:46 +0530

    added autocomplete off in input box

--- navbar.js
+++ navbar.js
@@ -5,7 +5,7 @@ const nav=()=>{
     <a href="./latest.html">lattest reciepe</a>
     <a href="./recipeoftheday.html">reciepe of the day</a>
     <button id="searchfood">search</button>
-    <input type="text" name="" id="search" placeholder="input text">`
+    <input autocomplete="off" type="text" name="" id="search" placeholder="input text">`
     document.body.insertBefore(div,document.body.firstChild);
 }
 export default nav;
